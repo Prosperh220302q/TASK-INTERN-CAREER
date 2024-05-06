8f2c6a73eab539e17a3421256e856043842de776
prosper <h220302q@something.com>
2024-05-06 14:53:34 +0200

    add new dependencies and styles for button component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import tsconfigPaths from 'vite-tsconfig-paths'
-import path from "path"
+import path from "path";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
